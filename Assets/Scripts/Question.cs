b0b5c23ea0d40ac3bbc1a16d50ee2de7d9fb8692
readinggirl <teresa.edinger@gmail.com>
2025-06-07 18:42:32 +0200

    questions.json added

--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -7,10 +7,6 @@ public class Question
     public string Text { get; set; }
     public List<String> Choices { get; set; }
     public int CorrectAnswerIndex { get; set; }
-    public int Points { get; set; }
-    
-    internal class Location {
-        public int LocationX { get; set; }
-        public int LocationY { get; set; }
-    }
+    public int LocationX { get; set; }
+    public int LocationY { get; set; }
 }
